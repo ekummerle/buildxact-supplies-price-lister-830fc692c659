e5d69db5fa2bfb3eddc66bed8695afbd61628435
ekummerle <ekummerle@users.noreply.github.com>
2021-06-19 20:27:22 +1000

    Add handlers for different file types.

---
+++ HumphriesLoader.cs
@@ -0,0 +1,33 @@
+﻿namespace buildxact_supplies
+{
+    /// <summary>
+    /// A class to handle loading a Humphries file.
+    /// </summary>
+    public class HumphriesLoader : PriceLoaderBase, IPriceLoader
+    {
+        #region Constructor
+
+        /// <summary>
+        /// The constructor for the class.
+        /// </summary>
+        /// <param name="filename">The filename of the file to load from.</param>
+        public HumphriesLoader(string filename) : base (filename)
+        {
+
+        }
+
+        #endregion
+
+        #region Functions
+
+        /// <summary>
+        /// A function to load the prices from file.
+        /// </summary>
+        public void LoadPrices()
+        {
+
+        }
+
+        #endregion
+    }
+}
---
+++ MegaCorpLoader.cs
@@ -0,0 +1,33 @@
+﻿namespace buildxact_supplies
+{
+    /// <summary>
+    /// A class to handling loading a MageCorp file.
+    /// </summary>
+    public class MegaCorpLoader : PriceLoaderBase, IPriceLoader
+    {
+        #region Constructor
+
+        /// <summary>
+        /// The constructor for the class.
+        /// </summary>
+        /// <param name="filename">The filename of the file to load from.</param>
+        public MegaCorpLoader(string filename) : base(filename)
+        {
+
+        }
+
+        #endregion
+
+        #region Functions
+
+        /// <summary>
+        /// A function to load the prices from file.
+        /// </summary>
+        public void LoadPrices()
+        {
+
+        }
+
+        #endregion
+    }
+}
