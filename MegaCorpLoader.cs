8e30d9058d1894c6ed5fbcfb3409a7c345b71160
ekummerle <ekummerle@users.noreply.github.com>
2021-06-19 20:30:48 +1000

    Add exchange rate to MegoCorpLoader constructor.

--- MegaCorpLoader.cs
+++ MegaCorpLoader.cs
@@ -5,15 +5,25 @@
     /// </summary>
     public class MegaCorpLoader : PriceLoaderBase, IPriceLoader
     {
+        #region Variables
+
+        /// <summary>
+        /// The exchange rate to use when working out prices.
+        /// </summary>
+        private decimal exchangeRate;
+
+        #endregion
+
         #region Constructor
 
         /// <summary>
         /// The constructor for the class.
         /// </summary>
+        /// <param name="exchangeRate">The exchange rate to use.</param>
         /// <param name="filename">The filename of the file to load from.</param>
-        public MegaCorpLoader(string filename) : base(filename)
+        public MegaCorpLoader(decimal exchangeRate, string filename) : base(filename)
         {
-
+            this.exchangeRate = exchangeRate;
         }
 
         #endregion
